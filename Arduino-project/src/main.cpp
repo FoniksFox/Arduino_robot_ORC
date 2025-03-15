4836caeee9a616a0c51362164200463070ce6584
Foniks <borisbeslimov@gmail.com>
2025-03-15 21:17:43 +0100

    Add motor testing

--- Arduino-project/src/main.cpp
+++ Arduino-project/src/main.cpp
@@ -11,6 +11,9 @@ int sensors[8] = {26, 25, 33, 32, 35, 34, 39, 36};
 LineSensor lineSensor(27, sensors);
 VelocitySensor velocitySensor1(16);
 VelocitySensor velocitySensor2(17);
+MotorController motorController(19, 14, 12, 21, 23, 22);
+Motor motor1(motorController, 1);
+Motor motor2(motorController, 2);
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,6 +22,11 @@ void setup() {
   lineSensor.init();
   velocitySensor1.init();
   velocitySensor2.init();
+  motorController.init();
+  motor1.init();
+  motor2.init();
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
   pinMode(4, INPUT);
 }
 
