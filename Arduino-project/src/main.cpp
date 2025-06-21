bc74a0f1c56f31586de1d3a4216a7d399b09c564
Foniks <borisbeslimov@gmail.com>
2025-06-21 13:59:50 +0200

    Updated pins

--- Arduino-project/src/main.cpp
+++ Arduino-project/src/main.cpp
@@ -6,18 +6,19 @@
 #include "components/VelocitySensor/VelocitySensor.h"
 
 // Components initialization
-DistanceSensor distanceSensor(13, 18);
-int sensors[8] = {26, 25, 33, 32, 35, 39, 34, 36};
+DistanceSensor distanceSensor(14, 12);
+int sensors[8] = {26, 25, 33, 32, 35, 34, 39, 36};
 LineSensor lineSensor(27, sensors);
-VelocitySensor velocitySensor1(16);
-VelocitySensor velocitySensor2(17);
-MotorController motorController(19, 14, 12, 21, 23, 22);
+VelocitySensor velocitySensor1(22);
+VelocitySensor velocitySensor2(23);
+MotorController motorController(17, 5, 16, 4, 19, 18);
 Motor motor1(motorController, 1);
 Motor motor2(motorController, 2);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  
   distanceSensor.init();
   lineSensor.init();
   velocitySensor1.init();
@@ -27,7 +28,7 @@ void setup() {
   motor2.init();
   motor1.setSpeed(255);
   motor2.setSpeed(-100);
-  pinMode(4, INPUT);
+  //pinMode(4, INPUT);
 }
 
 void loop() {
@@ -35,6 +36,8 @@ void loop() {
   Serial.println("Line: " + String(lineSensor.getLinePosition()));
   Serial.println("Velocity 1: " + String(velocitySensor1.getVelocity()));
   Serial.println("Velocity 2: " + String(velocitySensor2.getVelocity()));
-  Serial.println("Battery: " + String(analogRead(4)/42) + "%");
+  //Serial.println("Battery: " + String(analogRead(4)/42) + "%");
   delay(1000);
+  
+  Serial.println("Hello, World!");
 }
